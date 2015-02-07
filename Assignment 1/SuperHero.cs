c87a0c5eea8fedf0510c9f81f9dd80a5fc431407
kerenmenguito <kcee_charmaine@hotmail.com>
2015-02-06 23:44:23 -0500

    Second Commit

---
+++ Assignment 1/SuperHero.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class SuperHero : Hero
+    {
+        //PRIVATE PROPERTIES
+        private bool strength = true;
+        private bool speed = true;
+        private bool health = true;
+
+        public SuperHero(string name): base(name)
+        {
+            this.name = name;
+            this.showPowers();
+        }
+        public void showPowers() 
+        {
+            Console.WriteLine("Super Powers!");
+        }
+
+        //CONSTRUCTOR METHOD
+        public static void main(string[] args)
+        {
+            string[] superPowers = { "Super Speed", "Super Strength", "Body Armour", "Flight", "Fire Generation", "Weather Control" };
+            string[] tempArray = new string[superPowers.Length];
+            string[] finalArray = new string [6];
+
+            Random rnd = new Random(3);
+            int randomPowers;
+            for (int element=0; element < superPowers.Length; element++ )
+            {
+                tempArray[element] = superPowers[element];
+            }
+            int index = 0;
+            while (index < finalArray.Length)
+            {
+                randomPowers = generateRandomPowers(rnd, superPowers.Length); // Generate random number
+                if (tempArray[randomPowers] != "unavailable")
+                {
+                    finalArray[index] = tempArray[randomPowers];
+                    tempArray[randomPowers] = "unavailable";
+                    index++;
+                }
+            }
+            Console.WriteLine("Super Powers");
+            Console.WriteLine("------------");
+            
+            for (index = 0; index < superPowers.Length; index++)
+                {
+                    Console.WriteLine(superPowers[index]);
+                }
+
+            Console.WriteLine("New Super Powers");
+            Console.WriteLine("----------------");
+            for (index = 0; index < finalArray.Length; index++)
+                {
+                    Console.WriteLine(finalArray[index]);
+                }
+            Console.WriteLine();
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            }
+            private static int generateRandomPowers(Random rnd, int max)
+                {
+                    int number;
+                    number = rnd.Next(max);
+                    return number;
+                }
+
+    }
+}
